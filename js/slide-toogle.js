82643ba3b1ef70ec8f53a2880316af100720fbc2
Kanan Ahalpara <35570090+KananAhalpara@users.noreply.github.com>
2018-02-10 19:03:32 -0500

    1st-commit

---
+++ js/slide-toogle.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $("#donut-1").click(function(){
+        $("#slide-1").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#donut-2").click(function(){
+        $("#slide-2").slideToggle("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#donut-3").click(function(){
+        $("#slide-3").slideToggle("slow");
+    });
+});
